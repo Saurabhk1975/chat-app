18f663f4da78ab65756738c225175a3b4cee3efa
SAURABH KUMAR <87616133+Saurabhk1975@users.noreply.github.com>
2023-10-26 11:56:20 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,11 +1,28 @@
 const mongoose = require("mongoose");
+// here nodejstutorial is the name of database which  will be created in database
+mongoose
+  .connect("mongodb://127.0.0.1:27017/chatForInternship")
+  .then(() => {
+    console.log("connection established to database");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
-const chatSchema = new mongoose.Schema({
-  user: String,
-  message: String,
-  correctedMessage: String,
+const schema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  correctedMessage: {
+    type: String,
+  },
 });
 
-const Chat = mongoose.model("Chat", chatSchema);
-
-module.exports = Chat;
+// user details is inside the database name of collection
+const user = mongoose.model("user_details", schema);
+module.exports = user;
